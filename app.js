baf38428987cdc0e0a2588c671fb74b4501fed3b
Shahadat Hossen <shahadat.07.sh@gmail.com>
2023-09-22 17:05:26 +0600

    Done

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp');
+
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const routes = require("./routes/routes");
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+  });
+
+// 1) GLOBAL MIDDLEWARES
+
+//Set security HTTP headers
+app.use(helmet());
+
+
+//Limit request from the same API
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+})
+
+app.use(limiter);
+
+//Body parser, reading data from body into req.body
+app.use(express.json());
+
+
+//Data sanitization agains NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization agains XSS
+app.use(xss());
+
+//Prevent parameter pollution
+// app.use(hpp());
+
+app.use(bodyParser.json());
+
+//Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers)
+  next();
+});
+
+
+// 3) ROUTES
+app.use(routes);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ routes/routes.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const adminController = require("../controllers/adminController");
+const authController = require("../controllers/authController");
+
+router.get("/", userController.home);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
+router.get("/currentUser/:userId", authController.currentUser);
+router.patch("/updateMyPassword/:id", authController.updatePassword);
+
+router.post("/deposit", authController.protect, userController.deposit);
+router.post("/withdraw", authController.protect, userController.withdraw);
+router.post(
+  "/depositAdsReveneue",
+  authController.protect,
+  userController.depositAdsReveneue
+);
+router.post(
+  "/transferToAds",
+  authController.protect,
+  userController.transferBalanceToAds
+);
+router.post(
+  "/transferToGames",
+  authController.protect,
+  userController.transferBalanceToGames
+);
+
+//add prediction
+router.post(
+  "/add-prediction",
+  authController.protect,
+  userController.userPrediction
+);
+router.post("/ad-game-revenue/:id", userController.adGameRevenue);
+router.post("/send-mail", userController.mailSending);
+
+//get user selected teams
+router.get("/get-user-selected-teams/:id", userController.getSelectedTeams);
+router.post("/add-ad-revenue", userController.addingAdReveneue);
+router.post("/trackAd", userController.trackAd);
+router.post("/calculate-ads-revenue", userController.calculateAdsRevenue);
+
+//Admin Deal
+router.post("/admin/registration", adminController.registration);
+router.post("/admin/login", adminController.login);
+router.post("/admin/upload-informations", adminController.uploadInformations);
+router.get("/admin/get-informations", adminController.getInformations);
+router.post("/admin/upload-game", adminController.uploadGame);
+router.put("/admin/update-result/:id", adminController.updateResult);
+router.delete("/admin/remove-game/:id", adminController.removeGame);
+router.put("/admin/update-dollar-rate", adminController.updateDollarRate);
+router.put("/admin/update-game-rate", adminController.updateGameRate);
+router.put(
+  "/admin/update-withdraw-number",
+  adminController.updateWithdrawNumber
+);
+router.put("/admin/upload-ads-image", adminController.uploadAdsImage);
+router.put("/admin/ads-percentage", adminController.adsPercentage);
+
+router.get(
+  "/admin/pending-deposit-transactions",
+  adminController.getPendingDepositTransactions
+);
+
+router.get(
+  "/admin/pending-withdraw-transactions",
+  adminController.getPendingWithdrawTransactions
+);
+router.put("/admin/approve", adminController.approveTransaction);
+router.get("/admin/deposited-balances", adminController.depositedBalance);
+router.get("/admin/withdrawn-balances", adminController.withdrawnBalance);
+router.get("/admin/totalBalances", adminController.totalBalances);
+router.get("/admin/allUsers", adminController.allUsers);
+router.put("/admin/block-user/:userId", adminController.blockUser);
+router.put("/admin/unblock-user/:userId", adminController.unblockUser);
+
+module.exports = router;
